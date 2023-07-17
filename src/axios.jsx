9dc11c1b888f92d096808f74372ecbacf1471a88
NotAnsar <karroauch.ansar@gmail.com>
2023-07-17 22:56:07 +0100

    .env fixed

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:3000/api/v1';
+const url = import.meta.env.VITE_BACKEND_APP_URL;
 
 export const makeRequest = axios.create({
 	withCredentials: true,
