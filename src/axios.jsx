2a304b2f146c66c11b6e1993e9a638a54ad8dbaf
Ansar karrouach <72499382+NotAnsar@users.noreply.github.com>
2023-07-17 21:23:42 +0100

    Update axios.jsx

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-https://connectify-api-oza9.onrender.com/https://connectify-api-oza9.onrender.com/
+//https://connectify-api-oza9.onrender.com/https://connectify-api-oza9.onrender.com/
 const url = 'https://connectify-api-oza9.onrender.com/api/v1';
 
 export const makeRequest = axios.create({
